f2d1815e3c1869cfb2d1a1dee6f74d1122c69897
Joseph Arias <arias.joseph.a@gmail.com>
2018-09-27 23:12:30 -0400

    PHP Intro

---
+++ PHP_Intro/PHP_Intro.php
@@ -0,0 +1,33 @@
+
+What is PHP?
+- PHP: Hypertext Preprocessor
+- Server Side Programming / Scripting Language
+- Can be embedded directly within HTML (<?php?>)
+- Files use a "php" file extension
+
+How Does PHP Work?
+1 - Client makes a request
+2 - Server runs PHP/ extensions
+3 - Query Database if needed
+4 - Load other files if needed
+5 - Result is sent back to client
+
+What can PHP Do?
+- Create dyanamic page content 
+- Interact with files on the server 
+- Collect & process forms 
+- Send/ receive cookies 
+- Interact with databases 
+- Access control 
+- Encrypt data 
+
+What Can You Build With PHP?
+- Basic Websites 
+- Blog Type Websites 
+- Shopping Carts 
+- Social Networks 
+- Content Management Systems 
+- Membership Websites 
+- Search Engines 
+- Backend APIs
+
